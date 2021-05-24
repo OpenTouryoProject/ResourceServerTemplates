882f400eb28c7b56fc3e3764093137bb06c5dc8e
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2021-05-24 16:36:05 +0900

    Tweak.

--- rst_dev.bat
+++ rst_dev.bat
@@ -9,4 +9,6 @@
 @rem 当該フォルダに本バッチファイルをコピーして実行することを想定している。
 
 xcopy /E /Y /I "OpenTouryo\root\programs\CS\Frameworks\Infrastructure\Build_net48" "ResourceServerTemplates\root\programs\OpenTouryoAssemblies\Build_net48\"
-xcopy /E /Y /I "OpenTouryo\root\programs\CS\Frameworks\Infrastructure\Build_netcore30" "ResourceServerTemplates\root\programs\OpenTouryoAssemblies\Build_netcore30\"
+xcopy /E /Y /I "OpenTouryo\root\programs\CS\Frameworks\Infrastructure\Build_netcore50" "ResourceServerTemplates\root\programs\OpenTouryoAssemblies\Build_netcore50\"
+
+pause
