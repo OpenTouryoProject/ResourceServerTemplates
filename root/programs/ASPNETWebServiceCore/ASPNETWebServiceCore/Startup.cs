764c79edad8c157838d4c93c916ba07a5076532a
daisukenishino2 <nishi_74322014@ksj.biglobe.ne.jp>
2021-06-17 21:31:00 +0900

    https://github.com/OpenTouryoProject/OpenTouryo/issues/410

--- root/programs/ASPNETWebServiceCore/ASPNETWebServiceCore/Startup.cs
+++ root/programs/ASPNETWebServiceCore/ASPNETWebServiceCore/Startup.cs
@@ -79,12 +79,11 @@ namespace ASPNETWebServiceCore
                 // The default HSTS value is 30 days.
                 // You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                //app.UseHttpsRedirection();
             }
 
             // HttpContextのマイグレーション用
             app._UseHttpContextAccessor();
-
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
